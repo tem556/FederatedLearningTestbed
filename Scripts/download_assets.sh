e75f4b1d8ba69015a0840d40ffab0ffb0a2d8ff4
Thang Bui <buinnthang@gmail.com>
2022-06-12 07:50:49 -0400

    Allow batch download

--- Scripts/download_assets.sh
+++ Scripts/download_assets.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-mkdir testbed
-cd testbed
-
-wget http://$MY_IP/assets/Server-1.0-STABLE.jar
-wget http://$MY_IP/assets/Client-1.0-STABLE.jar
-
-mkdir testbed-client
-mkdir testbed-server
-cd testbed-server
-
-wget http://$MY_IP/assets/newmodel.zip
-wget http://$MY_IP/assets/cifar-10-binary.tar.gz
-tar -xvzf cifar-10-binary.tar.gz
-mv cifar-10-batches-bin cifar-10
+for _client in 1 2 3 4
+do
+	echo "ssh to $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_client)"
+	ssh -T $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_client) <<-EOL
+		[ -d testbed ] && rm -rf testbed
+		mkdir testbed
+		cd testbed
+		tmux new-session -d 'wget http://$MY_IP/assets/Server-1.1-SNAPSHOT.jar'
+		tmux new-session -d 'wget http://$MY_IP/assets/Client-1.1-SNAPSHOT.jar'
+		mkdir testbed-client
+		mkdir testbed-server
+		cd testbed-server
+		tmux new-session -d 'wget http://$MY_IP/assets/newmodel.zip'
+		tmux new-session -d 'wget http://$MY_IP/assets/cifar-10-binary.tar.gz && tar -xvzf cifar-10-binary.tar.gz && mv cifar-10-batches-bin cifar-10'
+	EOL
+done
