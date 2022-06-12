3f586af70a16c99c61d5fe16a482c0e4b7440de6
Thang Bui <buinnthang@gmail.com>
2022-06-12 08:29:40 -0400

    Run experiments with JRE 8

--- Scripts/run_experiment.sh
+++ Scripts/run_experiment.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 _server=01
-_client_start=02
-_client_end=03
 
 echo "ssh to $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_server)"
 ssh -T $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_server) <<-EOL
 	cd testbed
 	rm -rf testbed-client/**
 	rm -rf testbed-server/model*
-	tmux new-session -d 'java -jar Server-1.0-STABLE.jar --fl --minClients 2 --workdir ./testbed-server --datasetratio 1.0 --rounds 23'
+	tmux new-session -d '/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar Server-1.1-SNAPSHOT.jar --fl --minClients 3 --workdir ./testbed-server --datasetratio 1.0 --rounds 23'
 	tmux ls
 EOL
 
-for _client in 2 4
+for _client in 2 3 4
 do
 	echo "ssh to $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_client)"
 	ssh -T $(printf "tnbui@nsl-n%02d.qatar.cmu.local" $_client) <<-EOL
 		cd testbed
 		rm -rf testbed-client/**
 		rm -rf testbed-server/model*
-		tmux new-session -d 'java -jar Client-1.0-STABLE.jar --fl --nclients 1 --workdir ./testbed-client --host 172.20.216.42'
+		tmux new-session -d '/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar Client-1.1-SNAPSHOT.jar --fl --nclients 1 --workdir ./testbed-client --host 172.20.216.42'
 		tmux ls
 	EOL
 done
